b42f1a2a60190b8f0f27d634a906165288d7b413
Kanishk-pannu <kanishk.pannu03@gmail.com>
2018-11-19 22:21:12 +0530

    first commit

---
+++ numbers.py
@@ -0,0 +1,10 @@
+#there are several operaters used
+print(1+2)
+print(2-3)
+print(2*3)
+#for taking out powers
+print(2**3)
+print(4/5)
+#mod operater is used to find out thr remainder when two numbers are divided
+print(7%5)
+#for calculating square root
