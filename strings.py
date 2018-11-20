3ba71543fd9d1f32a16db4a38831490aad46bc79
Kanishk-pannu <kanishk.pannu03@gmail.com>
2018-11-20 16:19:22 +0530

    first commit

---
+++ strings.py
@@ -0,0 +1,10 @@
+#using /n 
+print("hello world \nmy name is kanishk and a I like to code")
+#using \t
+print("hello world \tmy name is kanishk and a I like to code")
+#using len function
+a='hello my name is kanishk pannu'
+len(a)
+#using indexing grabbing something of a string
+sting1='hello world this kanishk pannu'
+sting1[6]
