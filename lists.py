7559b29518ab3832fdfd277ac46c3b0a1a86fcf4
Kanishk-pannu <kanishk.pannu03@gmail.com>
2018-11-22 15:14:13 +0530

    first commit

---
+++ lists.py
@@ -0,0 +1,14 @@
+#using append method
+ulist=['the','weather','is','nice','today']
+ulist.append("let's play")
+print(ulist)
+#using pop method
+ulist.pop()
+print(ulist)
+#using reverse method
+klist=[4,5,7,3,5,7,3,67,4]
+klist.reverse()
+print(klist)
+#using sort method
+klist.sort()
+print(klist)
