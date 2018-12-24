2d2960da76bf680749fb74576393b1b6e42891f9
Kanishk-pannu <kanishk.pannu03@gmail.com>
2018-12-24 22:39:13 +0530

    first commit

---
+++ statements.py
@@ -0,0 +1,22 @@
+#using if statement
+if True:
+    print ('ITS TRUE')
+
+a=2
+if a>=5:
+    print('a is not smaller than 5')
+else:
+    print('a is smaller than 5')
+
+
+word='watchman'
+if word=='gardner':
+    print('i love flowers')
+elif word=='watchman':
+    print('security is my duty')
+elif word=='mechanic':
+    print('love cars')
+elif word=='programmer':
+    print('love python')
+else:
+    print('what kind of job you have')
